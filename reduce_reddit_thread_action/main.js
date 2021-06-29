4de1f6a0c9d8fe494903f350a7e551320ca40c56
srijithprabhu <prabhu.srijith@gmail.com>
2021-06-28 20:22:26 -0500

    write reduce reddit threads.

--- reduce_reddit_thread_action/main.js
+++ reduce_reddit_thread_action/main.js
@@ -1,3 +1,65 @@
+const Cloudant = require("@cloudant/cloudant");
+
+const REDDIT_URL_PREFIX = "https://www.reddit.com"
+
+function updateDestinationRedditThread(elements, params) {
+    const cloudant_url = params["cloudant_url"];
+    const cloudant_apikey = params["cloudant_apikey"];
+    const doc_id = params["id"];
+    const cloudant = Cloudant({url: cloudant_url, maxAttempt: 5, plugins: [{ iamauth: { iamApiKey: cloudant_apikey } }, { retry: { retryDelayMultiplier: 4 } }]});
+    const db = cloudant.use(params["destination_db"]);
+    return db.get(doc_id).catch((err) => {
+        console.log(err);
+    }).then((doc) => {
+        if (!doc) {
+            doc = {}
+        }
+        doc._id = element_id;
+        doc.threads = elements;
+        return db.insert(doc);
+    });
+}
+
+function readSourceRedditThread(params) {
+    const cloudant_url = params["cloudant_url"];
+    const cloudant_apikey = params["cloudant_apikey"];
+    const element_id = params["id"];
+    const cloudant = Cloudant({url: cloudant_url, maxAttempt: 5, plugins: [{ iamauth: { iamApiKey: cloudant_apikey } }, { retry: { retryDelayMultiplier: 4 } }]});
+    const db = cloudant.use(params["dbname"]);
+    return db.get(element_id).then((doc) => {
+        return doc.thread;
+    });
+}
+
+function reduceRedditThread(childThread) {
+    let result = {};
+    let data = child.data;
+
+    result.title = data.title;
+    result.text = data.selftext;
+    result.url = `${REDDIT_URL_PREFIX}${data.permalink}`;
+    result.upvotes = data.ups;
+    result.downs = data.downs;
+    result.link_flair = data.link_flair_text;
+    return result;
+}
+
+function reduceRedditThreads(thread) {
+    let result = thread.data.children.map((child) => {
+        return reduceRedditThread(child);
+    });
+    return result;
+}
+
 function main(params) {
-    console.log(params);
+    const destination_dbname = params["destination_db"];
+    const source_dbname = params["dbname"];
+    const source_db = cloudant.use(db_name);
+    const source_id = params["id"];
+    return readSourceRedditThread(params)
+        .then((thread) => {
+            return reduceRedditThreads(thread);
+        }).then((reducedThread) => {
+            return updateDestinationRedditThread(reducedThread, params);
+        })
 }
