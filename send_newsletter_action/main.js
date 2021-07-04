c7a2b1acf010c2f47a89f838493f5e0a3a8ea111
Srijith Prabhu <prabhu.srijith@gmail.com>
2021-07-03 23:38:44 -0500

    Creating the mapping.

--- send_newsletter_action/main.js
+++ send_newsletter_action/main.js
@@ -8,12 +8,20 @@ function getAllSubredditThreads(params) {
     const cloudant_dbname = params["dbname"];
     const cloudant = Cloudant({url: cloudant_url, maxAttempt: 5, plugins: [{ iamauth: { iamApiKey: cloudant_apikey } }, { retry: { retryDelayMultiplier: 4 } }]});
     const db = cloudant.use(cloudant_dbname);
-    return db.list();
+    return db.list({include_docs}).then((response) => {
+        let results = response.rows;
+        return results.reduce((result, row) => {
+            const subreddit = row._id;
+            const threads = row.threads;
+            result[subreddit] = threads;
+            return result;
+        }, {});
+    });
 }
 
 function main(params) {
     return getAllSubredditThreads(params)
         .then((subreddit_threads_map) => {
-            console.log(subreddit_threads_map);
+            return subreddit_threads_map;
         })
 }
