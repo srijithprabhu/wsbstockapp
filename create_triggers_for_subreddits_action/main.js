be6292464d9ef166f5a44e52efb4617ee34ef9a2
srijithprabhu <prabhu.srijith@gmail.com>
2021-07-19 21:59:58 -0500

    Some fixes.

--- create_triggers_for_subreddits_action/main.js
+++ create_triggers_for_subreddits_action/main.js
@@ -1,7 +1,6 @@
 const Openwhisk = require('openwhisk');
 
-const WHISK_ALARM_FEED_PATH = "/whisk.system/alarms/alarm"
-const CRON_FORMAT = "%d %d * * *";
+const WHISK_ALARM_FEED_PATH = "/whisk.system/alarms/alarm";
 
 function getListOfSubreddits(users) {
     let resultMap = {};
@@ -25,10 +24,11 @@ function setupSubredditTriggersAndRules(subreddits, whisk) {
     const namespace = "subreddit";
     const actionName = "get-subreddit-top-threads";
     let triggers = subreddits.map((subreddit) => {
-        const feedParams = {cron: CRON_FORMAT.format(startTime.getUTCMinutes(), startTime.getUTCHours()), trigger_payload: {subreddit}};
-        const triggerName = "subreddit-caller-trigger-%d-%d".format(startTime.getUTCHours(), startTime.getUTCMinutes());
-        const ruleName = "subreddit-caller-rule-%d-%d".format(startTime.getUTCHours(), startTime.getUTCMinutes());
-        const feedName = "subreddit-caller-feed-%d-%d".format(startTime.getUTCHours(), startTime.getUTCMinutes());
+        const cronTab = `${startTime.getUTCMinutes()} ${startTime.getUTCHours()} * * *`;
+        const feedParams = {cron:cronTab, trigger_payload: {subreddit}};
+        const triggerName = `subreddit-caller-trigger-${startTime.getUTCHours()}-${startTime.getUTCMinutes()}`;
+        const ruleName = `subreddit-caller-rule-${startTime.getUTCHours()}-${startTime.getUTCMinutes()}`;
+        const feedName = `subreddit-caller-feed-${startTime.getUTCHours()}-${startTime.getUTCMinutes()}`;
         const nextMinute = startTime.getUTCMinutes() + 1;
         const nextHour = startTime.getUTCHours + Math.floor(nextMinute/60);
         startTime.setUTCMinutes(nextMinute % 60);
