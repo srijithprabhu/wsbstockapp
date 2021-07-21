378bc5fb5b6f7f9b6e907f4db1a7375827638eff
srijithprabhu <prabhu.srijith@gmail.com>
2021-07-20 20:50:15 -0500

    see if can also deploy the action to create the newsletter as well.

--- create_triggers_for_subreddits_action/main.js
+++ create_triggers_for_subreddits_action/main.js
@@ -20,45 +20,52 @@ function createOrUpdate(whiskResource, options) {
         })
 }
 
+function createTrigger(actionName, startTime, payload, whisk) {
+    const cronTab = `${startTime.getUTCMinutes()} ${startTime.getUTCHours()} * * *`;
+    const feedParams = {cron:cronTab, trigger_payload: payload};
+    const triggerName = `subreddit-caller-trigger-${startTime.getUTCHours()}-${startTime.getUTCMinutes()}`;
+    const ruleName = `subreddit-caller-rule-${startTime.getUTCHours()}-${startTime.getUTCMinutes()}`;
+    const annotations = {
+        wsbOrigin: true
+    };
+    return createOrUpdate(whisk.triggers,{
+        name: triggerName,
+        trigger: payload,
+        annotations: annotations
+    }).then((trigger) => {
+        return Promise.all([
+            createOrUpdate(whisk.feeds,{
+                feedName: WHISK_ALARM_FEED_PATH,
+                trigger: triggerName,
+                params: feedParams
+            }),
+            createOrUpdate(whisk.rules, {
+                name: ruleName,
+                action: actionName,
+                trigger: triggerName,
+                annotations: annotations
+            })
+        ]);
+    });
+}
+
 function setupSubredditTriggersAndRules(subreddits, whisk) {
     let startTime = new Date(Date.UTC(2021, 7, 16, 12, 0, 0));
     const package = "reddit";
     const actionName = `${package}/get-subreddit-top-threads`;
+    const newsletterActionName = `${package}/send-newsletter-action`;
     let triggers = subreddits.map((subreddit) => {
-        const cronTab = `${startTime.getUTCMinutes()} ${startTime.getUTCHours()} * * *`;
-        const feedParams = {cron:cronTab, trigger_payload: {subreddit}};
-        const triggerName = `subreddit-caller-trigger-${startTime.getUTCHours()}-${startTime.getUTCMinutes()}`;
-        const ruleName = `subreddit-caller-rule-${startTime.getUTCHours()}-${startTime.getUTCMinutes()}`;
-        const feedName = `subreddit-caller-feed-${startTime.getUTCHours()}-${startTime.getUTCMinutes()}`;
+        const payload = {subreddit: subreddit};
+        const trigger = createTrigger(actionName, startTime, payload, whisk);
         const nextMinute = startTime.getUTCMinutes() + 1;
         const nextHour = startTime.getUTCHours + Math.floor(nextMinute/60);
         startTime.setUTCMinutes(nextMinute % 60);
         startTime.setUTCHours(nextHour);
-        const payload = {subreddit};
-        const annotations = {
-            wsbOrigin: true
-        };
-        return createOrUpdate(whisk.triggers,{
-            name: triggerName,
-            trigger: payload,
-            annotations: annotations
-        }).then((trigger) => {
-            return Promise.all([
-                createOrUpdate(whisk.feeds,{
-                    feedName: WHISK_ALARM_FEED_PATH,
-                    trigger: triggerName,
-                    params: feedParams
-                }),
-                createOrUpdate(whisk.rules, {
-                    name: ruleName,
-                    action: actionName,
-                    trigger: triggerName,
-                    annotations: annotations
-                })
-            ]);
-        });
+        return trigger;
     });
-    return Promise.all(triggers);
+    return Promise.all(triggers).then((results) => {
+        return createTrigger(newsletterActionName, startTime, {}, whisk);
+    });
 }
 
 function main(params) {
@@ -68,5 +75,7 @@ function main(params) {
 
     const subreddits = getListOfSubreddits(params["users"]);
     const whisk = Openwhisk();
-    return setupSubredditTriggersAndRules(subreddits, whisk);
+    return setupSubredditTriggersAndRules(subreddits, whisk).then((results) => {
+        return {success: true}
+    });
 }
